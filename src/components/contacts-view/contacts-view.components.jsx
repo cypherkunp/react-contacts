5bd4df41527b0542bafa53a050e63c6744ae664a
Devvrat Shukla <devvratshukla@gmail.com>
2019-09-14 23:23:48 +0530

    fix: contacts-view not updating upon contact delete

--- src/components/contacts-view/contacts-view.components.jsx
+++ src/components/contacts-view/contacts-view.components.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
+
 import Menu from '../../components/menu/menu.component';
 import TableView from './../../components/table-view/tabel-view.component';
 
@@ -9,36 +11,34 @@ class ContactsView extends Component {
     this.setState({ contacts: this.props.data });
   };
 
+  componentDidUpdate = () => {
+    if (!_.isEqual(this.state.contacts, this.props.data)) this.setState({ contacts: this.props.data });
+  };
+
   handleSearch = query => {
-    console.log(query);
     this.setState({ searchQuery: query });
   };
 
-  /*  getContacts = query => {
-    let contacts = [...this.state.contacts];
-    contacts;
-  }; */
+  getFilteredContacts = (contacts, searchQuery) => {
+    return contacts.filter(
+      contact =>
+        contact.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        contact.address.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        contact.email.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        contact.company.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        contact.notes.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        contact.website.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        contact.phone.includes(searchQuery.toLowerCase())
+    );
+  };
 
   render() {
     const { onDelete } = this.props;
     const { contacts, searchQuery } = this.state;
-
     let filteredContacts = contacts;
-    if (!contacts) return null;
-    if (searchQuery) {
-      console.log(filteredContacts);
 
-      filteredContacts = contacts.filter(
-        contact =>
-          contact.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
-          contact.address.toLowerCase().includes(searchQuery.toLowerCase()) ||
-          contact.email.toLowerCase().includes(searchQuery.toLowerCase()) ||
-          contact.company.toLowerCase().includes(searchQuery.toLowerCase()) ||
-          contact.notes.toLowerCase().includes(searchQuery.toLowerCase()) ||
-          contact.website.toLowerCase().includes(searchQuery.toLowerCase()) ||
-          contact.phone.includes(searchQuery.toLowerCase())
-      );
-    }
+    if (!contacts) return null;
+    if (searchQuery) filteredContacts = this.getFilteredContacts(contacts, searchQuery);
 
     return (
       <React.Fragment>
