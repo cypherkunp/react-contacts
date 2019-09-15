e3ad45c2c703c4ea0e6d0191dff55b271918203a
Devvrat Shukla <devvratshukla@gmail.com>
2019-09-15 21:00:31 +0530

    Updates

---
+++ precache-manifest.0fa90a10c87e2e453a7df2d0514c4f0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa125a6cd4475eb81f1d4c17c195e893",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbce1c1ab1605f19458f",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "31d803e0e56e83da2f1a",
+    "url": "/static/css/main.063436a2.chunk.css"
+  },
+  {
+    "revision": "dbce1c1ab1605f19458f",
+    "url": "/static/js/2.2f6589c9.chunk.js"
+  },
+  {
+    "revision": "31d803e0e56e83da2f1a",
+    "url": "/static/js/main.8b5d9813.chunk.js"
+  },
+  {
+    "revision": "30ffebf719d55b36ed1d",
+    "url": "/static/js/runtime~main.012e051e.js"
+  }
+]);
